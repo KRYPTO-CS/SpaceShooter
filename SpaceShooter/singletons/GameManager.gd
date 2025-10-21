900f32d05e1fa18d6bed00ccf4b62cd68343df80
Jacob <jacobmpav@gmail.com>
2025-10-21 15:19:51 -0500

    Test of JS bridge

--- SpaceShooter/singletons/GameManager.gd
+++ SpaceShooter/singletons/GameManager.gd
@@ -6,5 +6,10 @@ func add_score(points: int):
 	score += points
 	print("Score:", score)  # debug
 
+	if Engine.has_singleton("JavaScript"):
+		JavaScriptBridge.eval("sendScoreToReact(" + str(score) + ");")
+		
 func reset_score():
 	score = 0
+	if Engine.has_singleton("JavaScript"):
+		JavaScriptBridge.eval("sendScoreToReact(" + str(score) + ");")
