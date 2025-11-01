6e0177ff7f9163c5bd1b93b93ee962412e3cd37c
Jacob <jacobmpav@gmail.com>
2025-11-01 15:37:00 -0500

    Bug fixes with communication

--- SpaceShooter/singletons/GameManager.gd
+++ SpaceShooter/singletons/GameManager.gd
@@ -1,34 +1,49 @@
 extends Node
 
 var score: int = 0
-var speed = 100
-var commCount = 0
+var speed: float = 100
+var commCount: int = 0
 
 func _ready():
+	print("GameManager ready")
+
+	# Send a test message to confirm game -> React works
 	JavaScriptBridge.eval("testMessage();")
 
+	# Inject function accessible from HTML/React
 	JavaScriptBridge.eval("""
 		window.incrementCommInGodot = function() {
-			GodotRuntime.sendMessage('incrementComm');
+			if (typeof GodotRuntime !== 'undefined' && GodotRuntime.onMessageFromJS) {
+				GodotRuntime.onMessageFromJS('incrementComm');
+			} else {
+				console.log('GodotRuntime not ready to receive message');
+			}
 		};
 	""")
 
-func _process(_delta):
+	var engine_interface = JavaScriptBridge.get_interface("engine")
+	if engine_interface:
+		engine_interface.connect("message", Callable(self, "_on_js_message"))
+		print("Connected JS message listener in Godot")
+	else:
+		print("Could not get JS engine interface")
+
+func _process(_delta: float) -> void:
 	speed += 0.01
 
-func add_score(points: int):
+func add_score(points: int) -> void:
 	score += points
-
 	JavaScriptBridge.eval("sendScoreToReact(" + str(score) + ");")
-		
-func reset_score():
+
+func reset_score() -> void:
 	score = 0
 	JavaScriptBridge.eval("sendScoreToReact(" + str(score) + ");")
-	
 
-func _on_js_message(message: String):
+func _on_js_message(message: String) -> void:
+	print("Message from JS:", message)
+	commCount += 1
+
 	if message == "incrementComm":
 		commCount += 1
-		print("CommCount:", commCount)
+		print("CommCount incremented to", commCount)
 		JavaScriptBridge.eval("sendScoreToReact('CommCount: ' + " + str(commCount) + ");")
-		
